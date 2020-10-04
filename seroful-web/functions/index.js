d50caaec63562434c64c0803dea58ac8a90b6404
Jalon Dodson <jalon.dodson@yahoo.com>
2020-10-04 17:17:50 -0500

    adjusted express server so that it communicates efficiently with firebase

--- seroful-web/functions/index.js
+++ seroful-web/functions/index.js
@@ -1,12 +1,23 @@
 const functions = require("firebase-functions");
+
+const admin = require("firebase-admin");
+
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: "https://seroful.firebaseio.com",
+});
+
+const db = admin.firestore();
+
 const express = require("express");
 
 const app = express();
 
 // /messages, /users
 
-app.get("/users", (req, res) => {
-  res.send("Is the API working?");
+app.get("/users", async (req, res) => {
+  const results = await db.collection("users").get();
+  res.send(results);
 });
 
 exports.api = functions.https.onRequest(app);
