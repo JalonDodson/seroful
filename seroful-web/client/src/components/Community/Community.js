40d44033bccc647b6ccee38b70ff0eeb0d448533
Chip Pickering <mykeyworks.2113@gmail.com>
2020-10-05 12:29:55 -0400

    setting general skeleton for possible components

---
+++ seroful-web/client/src/components/Community/Community.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export const Community = async (props) => {
+    return (
+        <>
+        //TODO: display communal forum page
+        Community foo
+        </>
+    )
+};
