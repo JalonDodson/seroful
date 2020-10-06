19581202ed8064ba7d604a7e8546df516d60f5a6
Zachary Baca <zachbaca@icloud.com>
2020-10-06 16:04:52 -0500

    started implementing material ui for profile card

--- seroful-web/client/src/components/Pages/Profile/Profile.js
+++ seroful-web/client/src/components/Pages/Profile/Profile.js
@@ -1,8 +1,58 @@
 import React from "react";
+import { makeStyles, ThemeProvider } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+import { deepPurple } from '@material-ui/core/colors';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+    height: 400,
+  },
+  avatar: {
+    display: 'flex',
+    marginLeft: '150px',
+  },
+  content: {
+    display: 'flex',
+    marginLeft: '100px',
+  },
+  purple: {
+    display: 'flex',
+    marginLeft: '150px',
+    color: theme.palette.getContrastText(deepPurple[500]),
+    backgroundColor: deepPurple[500],
+  },
+  main: {
+    marginTop: '30%',
+  }
+}));
 
 export const Profile = (props) => {
+  const classes = useStyles();
   return (
   <>
-    <h1>something here</h1>
+    <h1>Welcome To Your Profile!</h1>
+    <Card className={classes.root}>
+      <CardActionArea className={classes.main}>
+        <Avatar className={classes.purple}>S</Avatar>
+        <CardContent className={classes.content}>
+          <Typography gutterBottom variant="h5" component="h2">
+            My Name
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            This is my profile page.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    
   </>);
 };
