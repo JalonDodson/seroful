e73132ebedcb58fb8eec6ae26da70b1650cb1efc
Jalon Dodson <jalon.dodson@yahoo.com>
2020-10-08 09:57:54 -0500

    fix merge issues

--- seroful-web/client/src/components/Navigator/Navigator.js
+++ seroful-web/client/src/components/Navigator/Navigator.js
@@ -1,46 +1,32 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-<<<<<<< HEAD
-import { Home, Profile, Friends, Planner, Journal, Settings, NotFound } from "../Pages/index";
+import {
+  Home,
+  Profile,
+  Friends,
+  Planner,
+  Journal,
+  Settings,
+  NotFound,
+  Splash,
+} from "../Pages/index";
 
-export const Navigator = () => (
+import { newUser } from "../../store/store";
+import { useRecoilValue } from "recoil";
+export const Navigator = () => {
+  const userNew = useRecoilValue(newUser);
+
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" render={() => <Home />} />
+      <Route exact path="/" render={() => (userNew ? <Splash /> : <Home />)} />
       // TODO: Make route for profile /users/(username or uid)/profile
       <Route path="/profile" render={() => <Profile />} />
       // TODO: Make route for planner /users/(username or uid)/planner
-      <Route path='/friends' render={()=> <Friends />} />
+      <Route path="/friends" render={() => <Friends />} />
       <Route path="/planner" render={() => <Planner />} />
       <Route path="/journal" render={() => <Journal />} />
       <Route path="/settings" render={() => <Settings />} />
       <Route path="*" render={() => <NotFound />} />
     </Switch>
-  </BrowserRouter>
-);
-=======
-import { Home, NotFound, Planner, Profile, Splash } from "../Pages/index";
-
-import { newUser } from "../../store/store";
-import { useRecoilValue } from "recoil";
-
-export const Navigator = () => {
-  const userNew = useRecoilValue(newUser);
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route
-          exact
-          path="/"
-          render={() => (userNew ? <Splash /> : <Home />)}
-        />
-        // TODO: Make route for profile /users/(username or uid)/profile
-        <Route path="/profile" render={() => <Profile />} />
-        // TODO: Make route for planner /users/(username or uid)/planner
-        <Route path="/planner" render={() => <Planner />} />
-        <Route path="*" render={() => <NotFound />} />
-      </Switch>
-    </BrowserRouter>
-  );
+  </BrowserRouter>;
 };
->>>>>>> master
