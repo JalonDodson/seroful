864afa44c6c15faf044644b1c701a07b72250cde
Chip Pickering <mykeyworks.2113@gmail.com>
2020-10-08 11:26:28 -0400

    committing to pull from master: jalon had to fix an infinite loop somewhere

--- seroful-web/client/src/components/Pages/index.js
+++ seroful-web/client/src/components/Pages/index.js
@@ -1,6 +1,5 @@
 import { Home } from "./Home/Home";
 import { Profile } from "./Profile/Profile";
-<<<<<<< HEAD
 import { Friends } from './Friends/Friends';
 import { Planner } from "./Planner/Planner";
 import { Journal } from './Journal/Journal';
@@ -8,8 +7,3 @@ import { Settings } from './Settings/Settings';
 import { NotFound } from "./NotFound/NotFound";
 
 export { Home, Profile, Friends, Planner, Journal, Settings, NotFound };
-=======
-import { Splash } from "./Splash/Splash";
-
-export { Home, NotFound, Planner, Profile, Splash };
->>>>>>> master
