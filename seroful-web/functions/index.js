1de108f672f53d82d13823acb76fb6ba0bd84ad4
Chip Pickering <mykeyworks.2113@gmail.com>
2020-10-09 10:02:53 -0400

    fixing my dumbassness

--- seroful-web/functions/index.js
+++ seroful-web/functions/index.js
@@ -3,13 +3,14 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const credential = require("./seroful-firebase-adminsdk-ry93d-5b49e47b83.json");
 
+const url = require("url");
 /* WHEN IN DEV MODE ASSIGN CREDENTIAL TO admin.credential.cert(credential) 
   AND GET THE FILE FROM DISCORD
   when deploying to firebase (firebase deploy). set it to admin.credential.applicationDefault()
 */
 admin.initializeApp({
   // credential: admin.credential.cert(credential),
-  credential: admin.credential.applicationDefault(),
+  credential: admin.credential.cert(credential),
   databaseURL: "https://seroful.firebaseio.com",
 });
 
@@ -34,10 +35,8 @@ const decodeIDToken = async (req, res, next) => {
   if (req.headers.authorization) {
     if (req.headers.authorization.startsWith("Bearer ")) {
       const idToken = req.headers.authorization.split("Bearer ")[1];
-      console.log(idToken);
       try {
         const decodedToken = await admin.auth().verifyIdToken(idToken);
-        console.log(decodedToken);
         req["currentUser"] = decodedToken;
       } catch (err) {
         console.log(err);
@@ -50,14 +49,14 @@ const decodeIDToken = async (req, res, next) => {
 app.use(decodeIDToken);
 
 app.get("/users", async (req, res) => {
-  const emailQuery = req.url.split("email=")[1];
+  const searchParam = req.url.split("=")[1];
   const user = req["currentUser"];
 
   if (user) {
     try {
       await db
         .collection("users")
-        .doc(emailQuery)
+        .doc(searchParam)
         .get()
         .then((snapshot) => res.status(200).send(snapshot.data()));
     } catch (err) {
@@ -88,53 +87,62 @@ app.post("/users", async (req, res) => {
 });
 
 app.patch("/users", async (req, res) => {
-  const emailQuery = req.url.split("email=")[1];
-  const user = req["currentUser"];
-  console.log(emailQuery);
-  console.log(user, "test");
-
-  const userData = await db
-    .collection("users")
-    .doc(emailQuery)
-    .get()
-    .then((doc) => {
-      doc.data();
-      console.log(doc.data());
-    })
-    .catch((err) => console.log(err));
-
-  if (user) {
-    await db
-      .collection("users")
-      .doc(emailQuery)
-      .update({
-        username: req.body.username ? req.body.username : userData.username,
-        email: req.body.email ? req.body.email : userData.email,
-        displayName: req.body.displayName
-          ? req.body.displayName
-          : userData.displayName,
-        medicines: req.body.medicines ? req.body.medicines : userData.medicines,
-        illnesses: req.body.illnesses ? req.body.illnesses : userData.illnesses,
-        plans: req.body.plans ? req.body.plans : userData.plans,
-        journals: req.body.journals ? req.body.journals : userData.journals,
-      })
-      .then(() => res.status(201).send("Request successful!"))
-      .catch((err) => res.status(400).send(err));
-  } else {
-    res.status(403).send("User not authenticated.");
+  try {
+    await db.collection("users").doc(req.body.email).update({
+      username: req.body.username,
+      email: req.body.email,
+      displayName: req.body.displayName,
+      photoURL: req.body.photo,
+      medicines: req.body.medicines,
+      illnesses: req.body.illnesses,
+      plans: req.body.plans,
+    });
+    res.status(201).send("Request successful!");
+  } catch (err) {
+    console.log(err);
+    res.status(400).send("Request failed.");
   }
 });
 
-app.get("/journal", async (req, res) => {
+// app.get("/profile", (req, res) => {
+//   //TODO: nav to user profile
+// });
+// app.get('/profile', (req, res) => {
+//   //TODO: nav to user profile
+// });
+// app.post('/profile', (req, res) => {
+//   //TODO: nav to user profile
+// });
+// app.get('/friends', (req, res) => {
+//   //TODO: nav to user friend list
+// });
+// app.post('/friends', (req, res) => {
+//   //TODO: nav to user friend list
+// });
+// app.get('/planner', (req, res) => {
+//   //TODO: nav to planner for current user
+// });
+// app.post('/planner', (req, res) => {
+//   //TODO: post new planner content for current user
+// });
+app.get('/journal', async (req, res) => {  
   try {
-    await db.collection("users").get({ displayName: res.body.displayName });
+    await db.collection("users").get({displayName: res.body.displayName});
     res.status(200);
   } catch (error) {
     res.status(400);
     console.log(error);
   }
 });
-
+// app.post('/journal', (req, res) => {
+//   //TODO: add content to user journal
+// });
+// app.get('/settings', (req, res) => {
+//   //TODO: nav to user settings
+// });
+// app.post('/settings', (req, res) => {
+//   //TODO: make changes to user settings
+// });
 app.get("/video-chat", (req, res) => {
   res.redirect(`/video-chat/:${nanoid()}`);
 });
