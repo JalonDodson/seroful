7dd5ec95fc630f5ab4d96d08e9baf38a5330307a
Jalon Dodson <jalon.dodson@yahoo.com>
2020-10-11 23:26:00 -0500

    Finished journal (barring styling), near-completion on planner (again barring styling), hooked endpoints up for planner + journal

--- seroful-web/client/src/components/Pages/Journal/Journal.js
+++ seroful-web/client/src/components/Pages/Journal/Journal.js
@@ -1,39 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React, { createRef, useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
 import { PageDrawer } from "../../PageDrawer/PageDrawer";
 import { journalStyles } from "../../../styles/journalStyles";
-import { useRecoilState, useSetRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import { userState } from "../../../store/store";
-import { createEntry, getEntries } from '../../../util/api';
+import { createEntry, getEntries } from "../../../util/api";
 
-import {
-  Button,
-  TextField,
-  Typography,
-} from "@material-ui/core";
+import { Button, TextField, Typography } from "@material-ui/core";
 
-import MuiAccordion from '@material-ui/core/Accordion';
-import MuiAccordionSummary from '@material-ui/core/AccordionSummary';
-import MuiAccordionDetails from '@material-ui/core/AccordionDetails';
-import { withStyles } from '@material-ui/core/styles';
+import MuiAccordion from "@material-ui/core/Accordion";
+import MuiAccordionSummary from "@material-ui/core/AccordionSummary";
+import MuiAccordionDetails from "@material-ui/core/AccordionDetails";
+import { responsiveFontSizes, withStyles } from "@material-ui/core/styles";
 import firebase from "firebase/app";
 import "firebase/auth";
 
-let ent = '';
+let ent = "";
 export const Journal = () => {
   const styles = journalStyles();
   const Accordion = withStyles({
     root: {
-      border: '1px solid rgba(0, 0, 0, .125)',
-      boxShadow: 'none',
-      '&:not(:last-child)': {
+      border: "1px solid rgba(0, 0, 0, .125)",
+      boxShadow: "none",
+      "&:not(:last-child)": {
         borderBottom: 0,
       },
-      '&:before': {
-        display: 'none',
+      "&:before": {
+        display: "none",
       },
-      '&$expanded': {
-        margin: 'auto',
+      "&$expanded": {
+        margin: "auto",
       },
     },
     expanded: {},
@@ -41,17 +37,17 @@ export const Journal = () => {
 
   const AccordionSummary = withStyles({
     root: {
-      backgroundColor: 'rgba(0, 0, 0, .03)',
-      borderBottom: '1px solid rgba(0, 0, 0, .125)',
+      backgroundColor: "rgba(0, 0, 0, .03)",
+      borderBottom: "1px solid rgba(0, 0, 0, .125)",
       marginBottom: -1,
       minHeight: 56,
-      '&$expanded': {
+      "&$expanded": {
         minHeight: 56,
       },
     },
     content: {
-      '&$expanded': {
-        margin: '12px 0',
+      "&$expanded": {
+        margin: "12px 0",
       },
     },
     expanded: {},
@@ -63,23 +59,25 @@ export const Journal = () => {
     },
   }))(MuiAccordionDetails);
 
-  const [expanded, setExpanded] = useState('panel1');
-
+  const [expanded, setExpanded] = useState("panel-0");
+  const [isUpdated, setUpdated] = useState(false);
   const handleChange = (panel) => (event, newExpanded) => {
     setExpanded(newExpanded ? panel : false);
   };
 
   const [user, setUser] = useRecoilState(userState);
-  console.log(user);
 
-  // const entries = getEntries(user.email);
+  useEffect(() => {
+    return async () => {
+      await getEntries().then((resp) => {
+        console.log(resp);
+        setUser((x) => (x = { ...x, journals: resp }));
+        setUpdated(false);
+      });
+    };
+  }, [isUpdated]);
 
-  const entryHelper = () => {};
-
-   const assignNewEntry = (val) => {
-     ent = val;
-   };
-
+  const inputRef = createRef();
   return (
     <>
       <Helmet>
@@ -95,43 +93,60 @@ export const Journal = () => {
           <hr />
         </header>
         <div className={styles.inter}>
-        <div className={styles.pastEntries}>
-          {//TODO: instance.get(all of users entries and map through them to return these bastards on one side, new entry on the others)
-          }
-            <Accordion square expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-              <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-                <Typography>Collapsible Group Item #1</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                  sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing
-                  elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.
-              </Typography>
-              </AccordionDetails>
-            </Accordion>        
-            </div>
-        <div className={styles.form}>
-        <form>
-          <TextField
-            label="New Entry"
-            type="text"
-            variant="filled"
-            className={styles.newEntry}
-            onChange={(ev) => {assignNewEntry(ev.target.value)
-            
-            }}
-          />
-          <br />
-          <Button
-          className={styles.submit}
-            variant="contained"
-            onClick={(ev) => {
-              ev.persist()
-              createEntry(ent, user.email)}}
-          >Add Journal Entry</Button>
-        </form>
-        </div>
+          <div className={styles.pastEntries}>
+            {
+              //TODO: instance.get(all of users entries and map through them to return these bastards on one side, new entry on the others)
+            }
+            {user.journals.map((x, i) => {
+              const newDate = new Date(x.timestamp).toString();
+              return (
+                <Accordion
+                  square
+                  expanded={expanded === `panel-${i}`}
+                  onChange={handleChange(`panel-${i}`)}
+                >
+                  <AccordionSummary
+                    aria-controls={`panel${i}-content`}
+                    id={`panel${i}-header`}
+                  >
+                    <Typography>{`${newDate.split(" ")[1]} ${
+                      newDate.split(" ")[2]
+                    } ${newDate.split(" ")[4]}`}</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography>
+                      {console.log(x)}
+                      {x.entry}
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
+              );
+            })}
+          </div>
+          <div className={styles.form}>
+            <form>
+              <TextField
+                label="New Entry"
+                type="text"
+                variant="filled"
+                className={styles.newEntry}
+                multiline
+                inputRef={inputRef}
+              />
+              <br />
+              <Button
+                className={styles.submit}
+                variant="contained"
+                onClick={(ev) => {
+                  ev.persist();
+                  createEntry(inputRef.current.value);
+                  setUpdated(true);
+                }}
+              >
+                Add Journal Entry
+              </Button>
+            </form>
+          </div>
         </div>
       </div>
     </>
